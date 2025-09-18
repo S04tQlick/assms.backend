b5a55168a50ba3c8dcea33d5414827f575f30e94
S04tQlick <s04t.qlick@gmail.com>
2025-09-18 07:46:58 +0100

    ASSMS-45-VendorDtos kicked off

---
+++ assms.entities/DTOs/VendorDtos/CreateVendorDto.cs
@@ -0,0 +1,10 @@
+namespace assms.entities.DTOs.VendorDtos;
+
+public class CreateVendorDto
+{
+    public required string Name { get; set; }
+    public required string ContactName { get; set; }
+    public required string ContactPhone { get; set; }
+    public required string ContactEmail { get; set; }
+    public required string Address { get; set; } 
+}
