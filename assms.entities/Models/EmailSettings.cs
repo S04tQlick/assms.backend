c9742fd0c3671a7f3e5159e504024f27f6cc78d6
S04tQlick <s04t.qlick@gmail.com>
2025-09-18 07:23:26 +0100

    ASSMS-38-EmailSettingsDtos amended

---
+++ assms.entities/Models/EmailSettings.cs
@@ -0,0 +1,12 @@
+namespace assms.entities.Models;
+
+public abstract class EmailSettings
+{
+    public required string SmtpServer { get; set; }
+    public int Port { get; set; }
+    public required string SenderName { get; set; }
+    public required string SenderEmail { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public bool UseSsl { get; set; } 
+}
