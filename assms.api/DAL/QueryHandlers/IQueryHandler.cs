e223300060c7352938e41231584f523492b6aca2
S04tQlick <s04t.qlick@gmail.com>
2025-09-18 20:41:45 +0100

    ASSMS-4-QueryHandler implemeted

--- assms.api/DAL/QueryHandlers/IQueryHandler.cs
+++ assms.api/DAL/QueryHandlers/IQueryHandler.cs
@@ -2,14 +2,11 @@ using assms.entities.Models;
 
 namespace assms.api.DAL.QueryHandlers;
 
-public interface IQueryHandler
+public interface IQueryHandler<T> where T : BaseModel
 {
-    public interface IQueryHandler<T> where T : BaseModel
-    {
-        Task<int> CreateAsync(T entity);
-        Task<int> UpdateAsync(T entity);
-        Task<T?> GetByIdAsync(Guid id);
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<IEnumerable<T>> FindAllByDateAsync(DateTime date);
-    } 
+    Task<IEnumerable<T>> GetAllByDateAsync(DateTime date);
+    Task<T?> GetByIdAsync(Guid id);
+    Task<int> CreateAsync(T entity);
+    Task<int> UpdateAsync(T entity);
+    Task<int> DeleteAsync(Guid id);
 }
