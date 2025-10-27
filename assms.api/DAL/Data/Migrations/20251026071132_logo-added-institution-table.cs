c5cd47ed27c1d9988ab84daa51c7f3d4d76c2949
S04tQlick <s04t.qlick@gmail.com>
2025-10-27 06:28:55 +0000

    migration added

---
+++ assms.api/DAL/Data/Migrations/20251026071132_logo-added-institution-table.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace assms.api.DAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class logoaddedinstitutiontable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Institutions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SanityAssetId",
+                table: "Institutions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SanityMimeType",
+                table: "Institutions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Institutions");
+
+            migrationBuilder.DropColumn(
+                name: "SanityAssetId",
+                table: "Institutions");
+
+            migrationBuilder.DropColumn(
+                name: "SanityMimeType",
+                table: "Institutions");
+        }
+    }
+}
